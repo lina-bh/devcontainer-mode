2ddc525499b7a831a343e08f2d2e51eb9e4ca117
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-05-06 17:22:42 +0100

    squash! Add test-emacs.sh

--- test-emacs.sh
+++ test-emacs.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 : "${ENGINE=docker}"
 : "${EMACS=30.1}"
+: "${IMAGE=devcontainer-test}"
+INIT_VOLUME="devcontainer-test-elpa$EMACS"
+volume_tip() {
+  local images
+  images="$($ENGINE volume ls -q)"
+  if ! grep "$INIT_VOLUME" >/dev/null <<< "$images"; then
+    printf "to cache cask packages: %s volume create %s\n" "$ENGINE" "$INIT_VOLUME"
+  fi
+}
+volume_tip
 exec ${ENGINE} run \
      --rm \
-     --mount=type=volume,source=devcontainer-test-elpa"${EMACS}",target=/root/.emacs.d/elpa \
+     --mount=type=volume,source="$INIT_VOLUME",target=/root/.emacs.d \
      --mount=type=bind,source="$(dirname "$0")",target=/devcontainer.el \
      --workdir /devcontainer.el \
-     devcontainer-test:"${EMACS}" cask emacs --batch "$@"
+     "$([ "$ENGINE" = podman ] && printf "%s" "--security-opt=label=disable")" \
+     "${IMAGE}:${EMACS}" "$@"
